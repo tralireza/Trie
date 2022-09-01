ec9423788f8a95e1de82bf42942fb572c3a47e98
Alireza <26859725+tralireza@users.noreply.github.com>
2022-09-02 00:05:00 +0100

    Trie 648m Replace Words -> Trie

--- trie.go
+++ trie.go
@@ -1,6 +1,9 @@
 package Trie
 
-import "log"
+import (
+	"log"
+	"strings"
+)
 
 func init() {
 	log.SetFlags(0)
@@ -8,6 +11,47 @@ func init() {
 
 // 648m Replace Words
 func replaceWords(dictionary []string, sentence string) []string {
-	r := []string{}
-	return r
+	type Trie struct {
+		IsWord bool
+		Child  [26]*Trie
+	}
+
+	Insert := func(r *Trie, w string) {
+		for i := 0; i < len(w); i++ {
+			c := r.Child[w[i]-'a']
+			if c == nil {
+				c = &Trie{}
+				r.Child[w[i]-'a'] = c
+			}
+			r = c
+		}
+		r.IsWord = true
+	}
+
+	Search := func(r *Trie, w string) bool {
+		for i := 0; i < len(w); i++ {
+			c := r.Child[w[i]-'a']
+			if c == nil {
+				return false
+			}
+			r = c
+		}
+		return r.IsWord
+	}
+
+	trie := &Trie{}
+	for _, w := range dictionary {
+		Insert(trie, w)
+	}
+
+	R := strings.Split(sentence, " ")
+	for i, w := range R {
+		for l := 1; l < len(w); l++ {
+			if Search(trie, w[:l]) {
+				R[i] = w[:l]
+				break
+			}
+		}
+	}
+	return R
 }
